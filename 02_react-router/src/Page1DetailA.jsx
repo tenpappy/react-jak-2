d9d7a090ca50b721df9d3aa2d5c35f0c04c64585
tenpappy <tenpappy@gmail.com>
2021-03-11 07:27:10 +0900

    ルーティング完了

--- 02_react-router/src/Page1DetailA.jsx
+++ 02_react-router/src/Page1DetailA.jsx
@@ -1,9 +1,19 @@
-import React from "react";
+import { useLocation, useHistory } from "react-router-dom";
 
 export const Page1DetailA = () => {
+  const location = useLocation();
+  console.log(location);
+
+  const history = useHistory();
+
+  const onClickBack = () => {
+    history.goBack();
+  };
+
   return (
     <div>
       <h1>Page1DetailAページです</h1>
+      <button onClick={onClickBack}>戻る</button>
     </div>
   );
 };
