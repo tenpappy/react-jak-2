338e05a5d1e6e29611ee654d906e1d0711b7ef89
tenpappy <tenpappy@gmail.com>
2021-03-19 12:31:25 +0900

    03_atomic-design完了

--- 03_atomic-design/src/App.jsx
+++ 03_atomic-design/src/App.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import { Router } from "./router/Router";
 import { UserProvider } from "./providers/UserProvider";
+import { RecoilRoot } from "recoil";
 
 export const App = () => {
   return (
-    <UserProvider>
-      <Router />
-    </UserProvider>
+    <RecoilRoot>
+      <UserProvider>
+        <Router />
+      </UserProvider>
+    </RecoilRoot>
   );
 };
